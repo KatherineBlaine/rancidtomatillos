a31f73cbd51b91e367de853d009728b380a3512d
Katherine Blaine <blaine.katherine12@gmail.com>
2023-03-24 16:31:52 -0600

    Add error handling for network requests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,15 +11,25 @@ class App extends Component {
     this.state = {
       allMovies: [],
       main: true,
+      error: ''
     };
   }
 
   componentDidMount = () => {
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('No Such Path')
+        }
+      })
       .then((data) => {
         this.setState({ allMovies: data.movies });
-      });
+      })
+      .catch((err) => {
+        this.setState({ error: err.message})
+      })
   };
 
   changeView = () => {
@@ -31,18 +41,40 @@ class App extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <h1>Rancid</h1>
-        {!this.state.main && <button onClick={this.changeView}>Home</button>}
-        {this.state.main ? (
-          <Main movies={this.state.allMovies} onViewChange={this.changeView} />
-        ) : (
-          <MoviePage movie={moviePageSample} />
-        )}
-      </div>
-    );
+    if (this.state.error === '') {
+      return (
+        <div>
+          <h1>Rancid</h1>
+          {!this.state.main && <button onClick={this.changeView}>Home</button>}
+          {this.state.main ? (
+            <Main movies={this.state.allMovies} onViewChange={this.changeView} />
+          ) : (
+            <MoviePage movie={moviePageSample} />
+            )}
+        </div>);
+    } else {
+        return (
+          <div>
+            <h1>Rancid</h1>
+            <h2>{this.state.error}, sorry!</h2>
+          </div>
+        )
+    }
   }
 }
 
 export default App;
+
+
+
+
+
+        {/* {this.state.main && this.state.error === '' ? (
+          <Main movies={this.state.allMovies} onViewChange={this.changeView} />
+        ) : !this.state.main && this.state.error === '' ? (
+          <MoviePage movie={moviePageSample} />
+        ) : (
+        <div> 
+          <Main movies={this.state.allMovies} onViewChange={this.changeView} />
+          <h2>{this.state.error}</h2>
+        </div>) */}
