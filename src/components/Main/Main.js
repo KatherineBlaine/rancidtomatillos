f9de7d2092056de55c70388e75e304e56ccc3b84
Jesus Borjas <jesusbor006@gmail.com>
2023-03-23 19:59:44 -0500

    Add fetch call to app component to display api data

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,14 +3,19 @@ import Movie from "../Movie/Movie";
 import "./Main.css";
 
 const Main = (props) => {
-  // console.log(props.onViewChange)
-  const movieElements = props.movies.movies.map((movie) => {
-    return (
-      <Movie title={movie.title} rating={movie.average_rating} key={movie.id} viewChange={props.onViewChange}/>
-    );
-  });
-
-  return <div className="movie-container">{movieElements}</div>;
+  if (props.movies !== []) {
+    const movieElements = props.movies.map((movie) => {
+      return (
+        <Movie
+          title={movie.title}
+          rating={movie.average_rating}
+          key={movie.id}
+          viewChange={props.onViewChange}
+        />
+      );
+    });
+    return <div className="movie-container">{movieElements}</div>;
+  }
 };
 
 export default Main;
