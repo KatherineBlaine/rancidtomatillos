42af6810d77630b7f08935255d289925140c14bf
Jesus Borjas <jesusbor006@gmail.com>
2023-03-25 11:02:20 -0500

    Add poster images to main page and add styling to both pages

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,24 +1,29 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import "./MoviePage.css";
 
 const MoviePage = ({ movie }) => {
   return (
-    <div>
-      <img src={movie.movie.poster_path}></img>
-      <h3>{movie.movie.title}</h3>
-      <p>{movie.movie.average_rating}</p>
-      <p>{movie.movie.overview}</p>
-      <p>{movie.movie.genres}</p>
-      <p>{movie.movie.budget}</p>
-      <p>{movie.movie.revenue}</p>
-      <p>{movie.movie.runtime}</p>
-      <p>{movie.movie.tagline}</p>
+    <div className="movie-page">
+      <div className="left-content">
+        <img src={movie.movie.poster_path}></img>
+      </div>
+      <div className="right-content">
+        <h3>{movie.movie.title}</h3>
+        <p>{movie.movie.average_rating}</p>
+        <p>{movie.movie.overview}</p>
+        <p>{movie.movie.genres}</p>
+        <p>{movie.movie.budget}</p>
+        <p>{movie.movie.revenue}</p>
+        <p>{movie.movie.runtime}</p>
+        <p>{movie.movie.tagline}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MoviePage;
 
 MoviePage.propTypes = {
-  movie: PropTypes.object
-}
+  movie: PropTypes.object,
+};
