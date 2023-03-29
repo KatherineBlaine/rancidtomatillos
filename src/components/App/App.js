1b6628fdcc8cf3a1251f84e1d83bddbae1289c87
Jesus Borjas <jesusbor006@gmail.com>
2023-03-28 19:09:46 -0500

    Add routing change to different page and back to home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import MoviePage from "../MoviePage/MoviePage";
 import Main from "../Main/Main.js";
 import moviePageSample from "../../moviePageSample";
+import { Switch, Link, Route } from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -10,8 +11,8 @@ class App extends Component {
     this.state = {
       allMovies: [],
       main: true,
-      error: '',
-      isLoading: true
+      error: "",
+      isLoading: true,
     };
   }
 
@@ -19,43 +20,72 @@ class App extends Component {
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
       .then((response) => {
         if (response.ok) {
-          return response.json()
+          return response.json();
         } else {
-          throw new Error('No Such Path')
+          throw new Error("No Such Path");
         }
       })
       .then((data) => {
-        this.setState({ allMovies: data.movies, isLoading: false});
+        this.setState({ allMovies: data.movies, isLoading: false });
       })
       .catch((err) => {
-        this.setState({ error: err.message})
-      })
+        this.setState({ error: err.message });
+      });
   };
 
-  changeView = () => {
-    this.setState((prevState) => {
-      return {
-        main: !prevState.main,
-      };
-    });
+  changeView = (movieId, movieTitle) => {
+    console.log("Movie Clicked:", movieId);
+    // alert(`${movieTitle} was clicked`)
+    return movieId;
+  };
+
+  getMovieDetails = (id, title, rating) => {
+    this.changeView(id);
   };
 
   render() {
     return (
       <div>
         <h1>Rancid Tomatillos</h1>
-        {!this.state.main && <button onClick={this.changeView}>Home</button>}
-
-        {this.state.main ? (
-          <Main movies={this.state.allMovies} onViewChange={this.changeView} />
-        ) : (
-          <MoviePage movie={moviePageSample} />
-        )}
-
-        {this.state.isLoading && this.state.error === '' ? <h2>Loading</h2>
-        : this.state.error !== '' && <h2>{this.state.error}, sorry!</h2>}
-      </div>);
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Main
+              movies={this.state.allMovies}
+              onViewChange={this.changeView}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/:movieID"
+          render={() => (
+            <MoviePage
+              movie={this.state.allMovies}
+              movieDetails={this.getMovieDetails}
+            />
+          )}
+        />
+      </div>
+    );
   }
 }
 
 export default App;
+
+// Old Code (Conditional Rendering)
+
+// {!this.state.main && <button onClick={this.changeView}>Home</button>}
+
+//         {this.state.main ? (
+//           <Main movies={this.state.allMovies} onViewChange={this.changeView} />
+//         ) : (
+//           <MoviePage movie={moviePageSample} />
+//         )}
+
+//         {this.state.isLoading && this.state.error === "" ? (
+//           <h2>Loading</h2>
+//         ) : (
+//           this.state.error !== "" && <h2>{this.state.error}, sorry!</h2>
+//         )}
--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,29 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./MoviePage.css";
+import { Link } from "react-router-dom";
 
-const MoviePage = ({ movie }) => {
+const MoviePage = ({ movie, movieDetails }) => {
+  const getSpecificMovie = movie.find((movie) => movie.id === 1013860);
+
+  console.log(
+    "This is the movie clicked",
+    movie.find((movie) => movie.id === 1013860)
+  );
+
   return (
-    <div className="movie-page">
-      <div className="left-content">
-        <img src={movie.movie.poster_path}></img>
+    <main>
+      <Link to="/">Go Back</Link>
+      <div className="movie-page">
+        <div className="left-content">
+          <p>This is the ID: {getSpecificMovie.id}</p>
+          <img src={getSpecificMovie.poster_path} alt="Movie poster"></img>
+        </div>
+        <div className="right-content">
+          <h3>{getSpecificMovie.title}</h3>
+          <p>{getSpecificMovie.average_rating}</p>
+          <p>{getSpecificMovie.release_date}</p>
+        </div>
       </div>
-      <div className="right-content">
-        <h3>{movie.movie.title}</h3>
-        <p>{movie.movie.average_rating}</p>
-        <p>{movie.movie.overview}</p>
-        <p>{movie.movie.genres}</p>
-        <p>{movie.movie.budget}</p>
-        <p>{movie.movie.revenue}</p>
-        <p>{movie.movie.runtime}</p>
-        <p>{movie.movie.tagline}</p>
-      </div>
-    </div>
+    </main>
   );
 };
 
 export default MoviePage;
 
 MoviePage.propTypes = {
-  movie: PropTypes.object,
+  movie: PropTypes.array,
 };
+
+// {/* <h3>{movie.movie.title}</h3> */}
+//           {/* <p>{movie.movie.average_rating}</p> */}
+//           {/* <p>{movie.movie.overview}</p> */}
+//           {/* <p>{movie.movie.genres}</p> */}
+//           {/* <p>{movie.movie.budget}</p> */}
+//           {/* <p>{movie.movie.revenue}</p> */}
+//           {/* <p>{movie.movie.runtime}</p> */}
+//           {/* <p>{movie.movie.tagline}</p> */}
